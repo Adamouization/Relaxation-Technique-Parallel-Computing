e2ca6ca9352577c94171219456776a6dfb3b8542
Adamouization <aj645@bath.ac.uk>
2018-11-05 17:51:34 +0000

    Initialise a 2D square array of doubles:
    - use initial array of pointers, with each pointer looking at a 1D array
      made of random doubles
    - add top of file comments
    - use function definitions
    -

--- main.c
+++ main.c
@@ -1,7 +1,61 @@
-#include <stdio.h>
+/*
+ * CM30225 Parallel Computing
+ * Assessment Coursework 1
+ * Relaxation technique for solving differential equations in a shared memory architecture using pthreads
+ * Author: aj645
+ * Date: 19-Nov-2018
+ */
 
-int main()
-{
-   printf("Hello, World!\n");
-   return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#define dim 4
+
+
+// Function definitions
+double double_random(double low, double high);
+
+
+/*
+ * Program entry.
+ */
+int main() {
+	// allocate space for a 1D array of double pointers.
+	double **square_array = malloc(dim * sizeof(double*));
+	if (square_array == NULL) {
+		printf("square_array array is NULL\n");
+	}
+
+	// allocate space for multiple 1D arrays of doubles
+	for (int i = 0; i < dim; i++){
+		square_array[i] = malloc(dim * sizeof(double));
+		if (square_array[i] == NULL) {
+			printf("square_array array %d is NULL\n", i);
+		}
+	}
+
+	// populate arrays with random doubles
+	for (int i = 0; i < dim; i++){
+		for (int j = 0; j < dim; j++) {
+			square_array[i][j] = double_random(1.0, 10.0);
+		}
+	}
+
+	// print array the array
+	for (int i = 0; i < dim; i++) {
+ 		for (int j = 0; j < dim; j++) {
+ 			printf("%f ", square_array[i][j]);
+ 		}
+ 		printf("\n");
+ 	}
+
+ 	free(square_array);
+   	return 0;
+}
+
+
+/*
+ * Returns a random double ranging from 'low' to 'high'.
+ */
+double double_random (double low, double high) {
+    return ((double)rand() * ( high - low )) / (double)RAND_MAX + low;
 }
