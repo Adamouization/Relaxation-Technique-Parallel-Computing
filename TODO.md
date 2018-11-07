45de081151e2b16b7c23550ce18b3baa798f00bd
Adamouization <aj645@bath.ac.uk>
2018-11-07 13:07:05 +0000

    Finish sequential implementation

--- TODO.md
+++ TODO.md
@@ -2,11 +2,12 @@
 
 ## System-critical
 
-* [ ] Implement relaxation technique sequentially
+* [X] Implement relaxation technique sequentially
 * [ ] Implement relaxation technique in parallel using pthreads and run on Balena
 * [ ] Write report in LaTeX
 
 ## Code design
 
 * [X] Move all code from the main() to individual functions.
+* [ ] Move log print statements to own funciton.
 * [ ] Create a makefile for compiling the code with GCC then running it.
--- main.c
+++ main.c
@@ -15,13 +15,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 #define dim 4
 
 
 // Function definitions
 double ** create_square_array(void);
+// double ** relaxation(double **square_array, double precision);
 double ** relaxation(double **square_array, double precision);
-double ** average_array(double **square_array);
 void check_malloc(double **sq_array);
 double double_random(double low, double high);
 void print_initial_data(double **array, int dimension, int threads, double precision);
@@ -38,11 +40,10 @@ int main() {
 	int number_of_threads = 2;
 	double precision = 0.1;
 	print_initial_data(square_array, array_dimension, number_of_threads, precision);
-	
 
 	// iterate averaging until precision reached
-	square_array = average_array(square_array);
-	printf("updated array:\n");
+	square_array = relaxation(square_array, precision);
+	printf("\n------------------------ Final square array\n");
 	print_array(square_array);
 
  	free(square_array);
@@ -68,6 +69,7 @@ double ** create_square_array(void) {
 	// populate the array with random doubles
 	for (int i = 0; i < dim; i++) {
 		for (int j = 0; j < dim; j++) {
+			//sq_array[i][j] = rand() % 10;
 			sq_array[i][j] = double_random(1.0, 10.0);
 		}
 	}
@@ -82,35 +84,58 @@ double ** create_square_array(void) {
  * values.
  * Returns a new square array with the updated values.
  */
-double ** average_array(double **temp_array) {
-	for (int i = 0; i < dim; i++) {
-		for (int j = 0; j < dim; j++) {
-			if ( (!((i==0) || (i==dim-1))) && (!((j==0) || (j==dim-1))) ) {
-				// element to average
-				double element = temp_array[i][j]; 
+double ** relaxation(double **temp_array, double precision) {
+	bool is_above_precision = true;
+	int precision_counter = 0;
+	int iteration_counter = 0;
+	double difference = 0.0; // different between old and new value
+	int number_of_values_to_change = ((dim-2) * (dim-2));
 
-				// get 4 surrounding elements needed to average
-				double ele_left = temp_array[i][j-1];
-				double ele_right = temp_array[i][j+1];
-				double ele_up = temp_array[i-1][j];
-				double ele_down = temp_array[i+1][j];
+	while (is_above_precision) {
+		precision_counter = 0;
+		printf("\n------------------------ Iteration #%d\n", iteration_counter);
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				// filter out border values
+				if ( (!((i==0) || (i==dim-1))) && (!((j==0) || (j==dim-1))) ) {
+					// value to replace
+					double old_value = temp_array[i][j]; 
 
-				// print data
-				printf("element of interest: %f\n", element);
-				printf("ele_left: %f\n", ele_left);
-				printf("ele_right: %f\n", ele_right);
-				printf("ele_up: %f\n", ele_up);
-				printf("ele_down: %f\n", ele_down);
+					// get 4 surrounding values needed to average
+					double ele_left = temp_array[i][j-1];
+					double ele_right = temp_array[i][j+1];
+					double ele_up = temp_array[i-1][j];
+					double ele_down = temp_array[i+1][j];
 
-				// perform the calculation
-				double new_value = (ele_left + ele_right + ele_up + ele_down) / 4;
-				printf("New value = %f\n", new_value);
-				printf("\n");
+					// perform the calculation
+					double new_value = (ele_left + ele_right + ele_up + ele_down) / 4;
 
-				// replace the old value with the new one
-				temp_array[i][j] = new_value;
+					// check if difference is smaller than precision
+					difference = fabs(old_value - new_value);
+					if (difference < precision) {
+						precision_counter++;
+					}
+
+					// if difference smaller than precision for all values, stop
+					if (precision_counter == number_of_values_to_change) {
+						is_above_precision = false;
+					}
+
+					// replace the old value with the new one
+					temp_array[i][j] = new_value;
+
+					// print data
+					printf("Value to replace: %f\n", old_value);
+					/*printf("ele_left: %f\n", ele_left);
+					printf("ele_right: %f\n", ele_right);
+					printf("ele_up: %f\n", ele_up);
+					printf("ele_down: %f\n", ele_down);*/
+					printf("New value = %f\n", new_value);
+					printf("precision_counter: %d\n", precision_counter);
+				}
 			}
 		}
+		iteration_counter++;
 	}
 	return temp_array;
 }
@@ -141,12 +166,12 @@ double double_random (double low, double high) {
  * Prints the initial data values used to initiate the program.
  */
 void print_initial_data(double **array, int dimension, int threads, double precision) {
-	printf("Initial data:\n");
-	printf("Square array:\n");
-	print_array(array);
+	printf("------------------------ Initial data:\n");
 	printf("Array dimension: %d\n", dimension);
 	printf("Number of threads: %d\n", threads);
-	printf("Precision: %f\n\n", precision);
+	printf("Precision: %f\n", precision);
+	printf("Square array:\n");
+	print_array(array);
 }
 
 
