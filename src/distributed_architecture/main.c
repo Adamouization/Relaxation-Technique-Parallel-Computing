d002210e244f1ddfdb3b462a424cfc4e8d5ba98f
Adam Jaamour <aj645@bath.ac.uk>
2018-12-10 19:51:31 +0000

    Send chunk arrays to a single process

--- src/distributed_architecture/main.c
+++ src/distributed_architecture/main.c
@@ -26,9 +26,6 @@ void check_double_malloc(double** square_array) {
 }
 
 
- * Initialises a square array by creating an initial array of pointers, each 
- * looking at a 1D arrays of doubles.
- */
 double** initialise_square_array(int dim) {
 	long unsigned int dimension = (long unsigned int) dim;
 	double **sq_array;
@@ -56,6 +53,26 @@ double** initialise_square_array(int dim) {
 }
 
 
+double** create_new_array(int dim_x, int dim_y) {
+	long unsigned int dimension_x = (long unsigned int) dim_x;
+	long unsigned int dimension_y = (long unsigned int) dim_y;
+	double **arr;
+	int i, j;
+
+	// allocate space for a 1D array of double pointers.
+	arr = malloc(dimension_x * sizeof(double*));
+	check_double_malloc(arr);
+
+	// allocate space for multiple 1D arrays of doubles
+	for (i = 0; i < dimension_y; i++) {
+		arr[i] = malloc(dimension_y * sizeof(double));
+		check_double_malloc(arr);
+	}
+
+	return arr;
+}
+
+
 void print_array(int dimension, double** square_array) {
 	int i, j;
 	for (i = 0; i < dimension; i++) {
@@ -67,14 +84,43 @@ void print_array(int dimension, double** square_array) {
 }
 
 
+void print_non_square_array(int x, int y, double** square_array) {
+	int i, j;
+	for (i = 0; i < y; i++) {
+ 		for (j = 0; j < x; j++) {
+ 			printf("%f ", square_array[i][j]);
+ 		}
+ 		printf("\n");
+ 	}
+}
+
+
+double** select_chunk(int dimension, double** square_array, int start_row, int end_row) {
+	int difference = end_row - start_row + 1;
+	double** chunk_array = create_new_array(difference,dimension);
+	int i, j;
+	for (i = 0; i < difference; i++) {
+		for (j = 0; j < dimension; j++) {
+			chunk_array[i][j] = square_array[i][j];
+		}
+	}
+	
+	return chunk_array;
+}
+
 int main() {
-	bool DEBUG = false;
-	int dimension = 5;
+	int DEBUG = 1;
+	int dimension = 7;
 	double precision = 0.01f;
 	int world_size;
 	int world_rank;
 	
-    MPI_Init(NULL, NULL); // Initialize the MPI environment.
+    int rc = MPI_Init(NULL, NULL); // Initialize the MPI environment.
+	if (rc != MPI_SUCCESS) {
+		printf ("Error starting MPI program\n");
+		MPI_Abort(MPI_COMM_WORLD, rc);
+	}
+	
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Get the number of processes
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Get the rank of the process
 	
@@ -82,11 +128,65 @@ int main() {
 	if (world_rank == 0) {
 		double **square_array = initialise_square_array(dimension);
 		print_array(dimension, square_array);
-		printf("suck my rubber duck\n");
+		
+		double** chunk1 = select_chunk(dimension, square_array, 0, 3);
+		/*printf("\n");
+		print_non_square_array(dimension, 4, chunk1);*/
+		
+		
+
+		// allocate space for multiple 1D arrays of doubles
+		int i;
+		MPI_Send(&dimension, 1, MPI_INT, 1, 99, MPI_COMM_WORLD);
+		for (i = 0; i < 4; i++)
+		{
+			MPI_Send(chunk1[i], dimension, MPI_DOUBLE, 1, 99, MPI_COMM_WORLD);
+		}
+		
+		
+		
+		
+		/*
+		double** chunk2 = select_chunk(dimension, square_array, 2, 5);
+		MPI_send(chunk2, 1, MPI_DOUBLE, 2, 0, MPI_COMM_WORLD);
+		
+		double** chunk3 = select_chunk(dimension, square_array, 4, 6);
+		MPI_send(chunk3, 1, MPI_DOUBLE, 3, 0, MPI_COMM_WORLD);
+		*/
 	}
 	
-    // Print off a hello world message
-    printf("Hello world from processor #%d out of %d processors\n", world_rank, world_size);
-
+	// children process
+	else if (world_rank == 1){ // do rows 1 and 2
+		//MPI_Recv(&my_chunk1, 28, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		double** my_chunk1 = malloc(28 * sizeof(double));
+		int my_dimension;
+		MPI_Recv(&my_dimension, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("cazzo00000\n");
+		int i;
+		for (i = 0; i < 4; i++)
+		{
+			MPI_Recv(&my_chunk1[i], dimension, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			printf("cazzo\n");
+		}
+		
+		
+		
+		printf("Hello world from processor #%d out of %d processors\n", world_rank, world_size);
+		//MPI_Recv(&my_chunk1, 28, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("\n");
+		//print_non_square_array(dimension, 4, chunk1);
+	}
+	
+	else if (world_rank == 2){
+		// do rows 3 and 4
+		printf("Hello world from processor #%d out of %d processors\n", world_rank, world_size);
+	}
+	
+	else if (world_rank == 3){
+		// do rows 5
+		printf("Hello world from processor #%d out of %d processors\n", world_rank, world_size);
+	}
+	
     MPI_Finalize(); // Clean up the MPI environment.
+	return 0;
 }
